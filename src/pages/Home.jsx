2e28346fc9e5dffb15043b349d1a4c89946b98f7
Milos Mirkovic <milos.mirkovic7@gmail.com>
2024-11-12 14:08:50 +0100

    slika

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,6 +7,7 @@ import tara1 from "../img/tara1.jpg";
 import tara2 from "../img/tara2.jpg";
 import baba1 from "../img/baba1.jpg";
 import baba2 from "../img/baba2.jpg";
+import mi from "../img/mi.jpg";
 
 function Home() {
   return (
@@ -16,7 +17,7 @@ function Home() {
 
       <img src="" alt="" className="w-3/4 " />
       <h2 className="text-center text-xl">Onda su dosli oni</h2>
-      <p className="text-center">Nasi mali adnjlcici :) </p>
+      <p className="text-center">Nasi mali andjelcici :) </p>
       <div className="flex ">
         <img src={dusan1} alt="" className="w-1/2" />
         <img src={dusan2} alt="" className="w-1/2" />
@@ -31,6 +32,12 @@ function Home() {
         <img src={tara1} alt="" className="w-1/2" />
         <img src={tara2} alt="" className="w-1/2" />
       </div>
+
+      <p className="p-2">
+        U petak 15.11. posle skole, samo treba da spakujes stvari za 2 noci i 2
+        dana i idemo nas dvoje. <br /> Gde? Pa to je iznenadjenje , videces kad
+        stignemo. Imamo da se vozimo ali ne previse...
+      </p>
       <p className="text-center pb-2">
         Sve je sredjeno , nema nista da brines,{" "}
         <span className="text-center">baba</span>
@@ -38,16 +45,15 @@ function Home() {
         <img src={baba2} alt="" /> su upoznate i brinuce o njima.
         <br />
       </p>
-      <p className="p-2">
-        U petak 15.11. posle skole, samo treba da spakujes stvari za 2 noc i 2
-        dana i idemo nas dvoje. <br /> Gde? Pa to je iznenadjenje , videces kad
-        stignemo. Imamo da se vozimo ali ne previse...
-      </p>
+  
       <p className="p-2">Ponesi i kupaci za svaki slucaj</p>
 
-      <p className="p-2">Znam da prolazimo kroz tesku fazu ali samo hcou da znas ...</p>
+      <p className="p-2">
+        Znam da prolazimo kroz tesku fazu ali samo hocu da znas ...
+      </p>
 
       <h1 className="text-center text-xl pt-2 text-red-500">Volim te</h1>
+      <img src={mi} alt="" />
     </div>
   );
 }
